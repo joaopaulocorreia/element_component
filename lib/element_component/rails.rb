89a84bce670862bff5f4d74ac4f74d63bb4722aa
John <john@Johns-Mac-mini.local>
2026-05-19 20:49:53 -0300

    refactor: remove method_missing delegation from RailsHelpers

--- lib/element_component/rails.rb
+++ lib/element_component/rails.rb
@@ -13,17 +13,5 @@ module ElementComponent
     def helpers
       @view_context
     end
-
-    def respond_to_missing?(method_name, include_private = false)
-      @view_context.respond_to?(method_name, include_private) || super
-    end
-
-    def method_missing(method_name, ...)
-      if @view_context.respond_to?(method_name)
-        @view_context.public_send(method_name, ...)
-      else
-        super
-      end
-    end
   end
 end
