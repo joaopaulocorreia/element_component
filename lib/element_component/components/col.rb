8c6b08afcd736a016d8f3f1f999b1a4b3755aa6f
John <john@macmini.local>
2026-06-02 12:03:00 -0300

    feat: add dynamic methods to Col for post-initialization breakpoint configuration

--- lib/element_component/components/col.rb
+++ lib/element_component/components/col.rb
@@ -8,14 +8,29 @@ module ElementComponent
       def initialize(content = nil, col: nil, offset: nil, order: nil, **attributes, &)
         super("div", content, **attributes, &)
 
-        add_col_classes(col)
-        add_classes_from_breakpoints("offset", offset)
-        add_classes_from_breakpoints("order", order)
+        apply_col_classes(col)
+        apply_breakpoint_classes("offset", offset)
+        apply_breakpoint_classes("order", order)
+      end
+
+      def col(value)
+        apply_col_classes(value)
+        self
+      end
+
+      def offset(value)
+        apply_breakpoint_classes("offset", value)
+        self
+      end
+
+      def order(value)
+        apply_breakpoint_classes("order", value)
+        self
       end
 
       private
 
-      def add_col_classes(col)
+      def apply_col_classes(col)
         if col.nil?
           add_attribute(class: "col")
         else
@@ -23,7 +38,7 @@ module ElementComponent
         end
       end
 
-      def add_classes_from_breakpoints(prefix, value)
+      def apply_breakpoint_classes(prefix, value)
         breakpoint_classes(prefix, value).each { |klass| add_attribute(class: klass) }
       end
     end
