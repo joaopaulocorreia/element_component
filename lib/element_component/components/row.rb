40989aab4f9d6afe4f4df2fb7526017359d97979
John <john@macmini.local>
2026-06-02 19:24:56 -0300

    refactor: remove dynamic methods from Row component

--- lib/element_component/components/row.rb
+++ lib/element_component/components/row.rb
@@ -15,26 +15,6 @@ module ElementComponent
         apply_breakpoint_classes("gy", gutter_y)
       end
 
-      def cols(value)
-        apply_breakpoint_classes("row-cols", value)
-        self
-      end
-
-      def gutter(value)
-        apply_breakpoint_classes("g", value)
-        self
-      end
-
-      def gutter_x(value)
-        apply_breakpoint_classes("gx", value)
-        self
-      end
-
-      def gutter_y(value)
-        apply_breakpoint_classes("gy", value)
-        self
-      end
-
       private
 
       def apply_breakpoint_classes(prefix, value)
